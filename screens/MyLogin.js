ee879d96884c6896e42dc72b91869172bce3a437
Azen Moktan <moktanazen@gmail.com>
2023-03-27 20:43:05 +0545

    Pokecard completed

---
+++ screens/MyLogin.js
@@ -0,0 +1,14 @@
+import { Text, StyleSheet, View,Button } from 'react-native'
+import React, { Component } from 'react'
+
+export default class MyLogin extends Component {
+  render() {
+    return (
+      <View>
+        <Text>MyLogin</Text>
+      </View>
+    )
+  }
+}
+
+const styles = StyleSheet.create({})
